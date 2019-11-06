1e4669948e26b28b703cf8a01e929de01a53b80e
Euan Thoms <euan@potensol.com>
2019-11-07 00:08:23 +0800

    Add code to rename 10-evdev.conf to 45-evdev.conf to it takes effect over libinput.

--- scripts/piscreen-setup.sh
+++ scripts/piscreen-setup.sh
@@ -29,6 +29,8 @@ apt install -y fbi xserver-xorg-input-evdev
 #apt install libts-bin evtest xinput python-dev python-pip xinput-calibrator
 #pip install evdev
 
+BOOT_CONFIG_CHANGES="dtparam=spi=on\ndtoverlay=piscreen,speed=24000000,rotate=90"
+
 EVDEV_CALIBRATION_XORG_CONF="
 Section \"InputClass\"
     Identifier \"calibration\"
@@ -40,20 +42,18 @@ Section \"InputClass\"
 EndSection
 "
 
-BOOT_CONFIG_CHANGES="dtparam=spi=on\ndtoverlay=piscreen,speed=24000000,rotate=90"
-
 echo "\n${YELL}Enabling SPI and PiScreen in '${ORNG}/boot/config.txt${YELL}'...${NRML}\n"
-
 sed -i "s|^[#]dtparam=spi=.*|${BOOT_CONFIG_CHANGES}|" /boot/config.txt
 
 echo "\n${YELL}Changing /dev/fb0 to /dev/fb1 in '${ORNG}/usr/share/X11/xorg.conf.d/99-fbturbo.conf${YELL}'...${NRML}\n"
-
 sed -i "s|/dev/fb0|/dev/fb1|" /usr/share/X11/xorg.conf.d/99-fbturbo.conf
 
 echo "\n${YELL}Adding xorg config file for touchscreen calibration in\n'${ORNG}/etc/X11/xorg.conf.d/99-touchscreen-calibration.conf${YELL}'...${NRML}\n"
-
 if [ ! -d /etc/X11/xorg.conf.d ]
 then
 	mkdir -p /etc/X11/xorg.conf.d
 fi
 echo "${EVDEV_CALIBRATION_XORG_CONF}" > /etc/X11/xorg.conf.d/99-touchscreen-calibration.conf
+
+echo "\n${YELL}Enabling SPI and PiScreen in '${ORNG}/boot/config.txt${YELL}'...${NRML}\n"
+mv /usr/share/X11/xorg.conf.d/10-evdev.conf /usr/share/X11/xorg.conf.d/45-evdev.conf
